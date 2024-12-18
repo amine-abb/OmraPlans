d6b17f1a490d8ed79c7c582503ed2e456a67ede2
amine-abb <amineabdev@gmail.com>
2024-12-18 09:34:32 +0100

    the logic seems work well it just need to adjust the look

--- app/src/main/java/com/example/omraplan/MainActivity.kt
+++ app/src/main/java/com/example/omraplan/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.omraplan
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,7 +16,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -23,10 +31,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,7 +48,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             OmraPlanTheme {
-                OmraScreen()
+                Scaffold (modifier = Modifier.fillMaxSize()){ innerPadding ->
+                    OmraScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    ) }
+
             }
         }
     }
@@ -60,7 +74,7 @@ fun OmraScreen(modifier: Modifier = Modifier) {
     ) {
         DisplayPlan(currentPlan = currentPlan)
         Spacer(modifier = Modifier.padding(10.dp))
-        TextInfo(infoPlan = infoPlan, infoTime = infoTime)
+        TextInfo(modifier = Modifier, infoPlan = infoPlan,infoTime = infoTime )
         Spacer(modifier = Modifier.padding(10.dp))
         Row(modifier = Modifier.fillMaxWidth(), Arrangement.SpaceEvenly) {
             Button(onClick = {
@@ -89,7 +103,34 @@ fun OmraScreen(modifier: Modifier = Modifier) {
                     }
                 }
             }) {
-                Text(text = "back")
+                Text(text = "Back")
+            }
+            Button(onClick = {
+                when (currentPlan) {
+                    firstplan -> {
+                        currentPlan = secondplan
+                        infoPlan = R.string.infoFeb
+                        infoTime = R.string.timeFeb
+                    }
+                    secondplan -> {
+                        currentPlan = thirdplan
+                        infoPlan =R.string.infochaarban
+                        infoTime = R.string.timechaaban
+
+                    }
+                    thirdplan -> {
+                        currentPlan = fourthplan
+                        infoPlan =R.string.inforamdan
+                        infoTime = R.string.tameramdan
+                    }
+                    else -> {
+                        currentPlan = firstplan
+                        infoPlan =R.string.infoJun
+                        infoTime = R.string.timeJun
+                    }
+                }
+            }) {
+                Text(text = "Next")
             }
         }
 
@@ -101,25 +142,36 @@ fun DisplayPlan(
     modifier: Modifier=Modifier,
     @DrawableRes currentPlan: Int
 ) {
+    Surface(
+        modifier = modifier.padding(20.dp),
+        border = BorderStroke(2.dp, color = Color.Transparent), shadowElevation = 15.dp)
+    {
     Image(
         painter = painterResource(currentPlan),
         contentDescription = null,
-        contentScale = ContentScale.Crop
+        contentScale = ContentScale.Fit,
+        modifier = Modifier
+            .size(400.dp)
+            .padding(20.dp, 25.dp, 20.dp, 15.dp)
     )
-}
+}}
 
 @Composable
-fun TextInfo(@StringRes infoPlan: Int, @StringRes infoTime: Int) {
-    Text(text = stringResource(infoPlan), fontSize = 35.sp)
-    Text(text = stringResource(infoTime), fontWeight = FontWeight.Bold, fontSize = 20.sp)
+fun TextInfo(modifier: Modifier=Modifier,@StringRes infoPlan: Int, @StringRes infoTime: Int) {
+    Card(modifier = Modifier.padding(20.dp,30.dp) , colors = CardDefaults.cardColors(
+        containerColor = MaterialTheme.colorScheme.surfaceVariant ) ) {
+        Column (
+            modifier = Modifier.fillMaxWidth()
+                .padding(20.dp) ,Arrangement.Center,Alignment.CenterHorizontally){
+    Text(text = stringResource(infoPlan), fontSize = 35.sp, fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
+    Text(modifier = Modifier,text = stringResource(infoTime), fontSize = 20.sp, textAlign = TextAlign.Center)
 
-}
+}}}
 
-
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    OmraPlanTheme {
+fun OmraPlanPreview(){
+    OmraPlanTheme{
         OmraScreen()
     }
 }
